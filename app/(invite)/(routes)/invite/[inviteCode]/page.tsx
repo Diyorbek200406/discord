6157364a8fc9c465d4d30c2d12989ee28fef5019
Erkinov Diyorbek <erkinovdiyorbek39@gmail.com>
2024-01-16 09:40:46 +0500

    invite modal is done

--- app/(invite)/(routes)/invite/[inviteCode]/page.tsx
+++ app/(invite)/(routes)/invite/[inviteCode]/page.tsx
@@ -15,14 +15,9 @@ const InviteCodePage = async ({ params }: InviteCodePageProps) => {
   if (!params.inviteCode) return redirect("/");
 
   const existingServer = await db.server.findFirst({ where: { inviteCode: params.inviteCode, members: { some: { profileId: profile.id } } } });
-
   if (existingServer) return redirect(`/servers/${existingServer.id}`);
 
-  const server = await db.server.update({
-    where: { inviteCode: params.inviteCode },
-    data: { members: { create: [{ profileId: profile.id }] } },
-  });
-
+  const server = await db.server.update({ where: { inviteCode: params.inviteCode }, data: { members: { create: [{ profileId: profile.id }] } } });
   if (server) return redirect(`/servers/${server.id}`);
 
   return null;
